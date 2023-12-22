f10a1118a54884da481e0acc01ab8271b6c73b0f
Naman Rajput <namanrajput801@gmail.com>
2023-12-22 12:35:21 +0530

    ui:integrate the chatgpt api

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,17 @@ import msgIcon from "./assets/message.svg";
 import home from "./assets/home.svg";
 import saved from "./assets/bookmark.svg";
 import rocket from "./assets/rocket.svg";
+import sendBtn from "./assets/send.svg";
+import userIcon from "./assets/user-icon.png";
+import gptImgLogo from "./assets/chatgptLogo.svg";
+import { sendMsgToOpenAI } from "./openAi";
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const handleSend = async () => {
+    const res = await sendMsgToOpenAI(input);
+    console.log(res);
+  };
   return (
     <div className="App">
       <div className="sideBar">
@@ -46,7 +56,52 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          <div className="chat">
+            <img src={userIcon} className="chatImg" alt="" />
+            <p className="txt">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Cupiditate quaerat libero dolor aut ex et molestiae! Rerum
+              accusantium in inventore minima excepturi atque eum illum alias
+            </p>
+          </div>
+          <div className="chat bot">
+            <img className="chatImg" src={gptImgLogo} alt="" />
+            <p className="txt">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor,
+              voluptatibus cum distinctio aspernatur, autem hic rem beatae,
+              cumque illum ipsa voluptatum dolore in neque suscipit. Itaque
+              atque repudiandae error nemo quae! Molestias, porro reprehenderit
+              blanditiis dicta quo eaque cupiditate magni nostrum quasi
+              laudantium quia minus qui deleniti, natus atque. Illum doloremque
+              distinctio quia unde reiciendis quam veritatis voluptatibus sunt
+              architecto, amet, praesentium, voluptates minima! Natus,
+              similique. Quisquam laborum nam facilis sapiente. Tempora,
+              voluptate sapiente! Natus similique cum maiores sint, eaque
+              corporis consectetur unde adipisci quas incidunt, animi non
+              dolorum. Adipisci corrupti nesciunt amet tempora ex alias sapiente
+              quos esse totam.
+            </p>
+          </div>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input
+              type="text"
+              placeholder="Send a Message..."
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            />
+            <button className="send" onClick={handleSend}>
+              <img src={sendBtn} alt="Send" />
+            </button>
+          </div>
+          <p>Travel Ai may produce incorrect result</p>
+        </div>
+      </div>
     </div>
   );
 }
